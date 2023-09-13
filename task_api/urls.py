44028a478a676a1f1c681c37651942251cba456b
Oleksii <laziutaoleksij@gmail.com>
2023-09-13 23:51:27 +0300

    urls changes

--- task_api/urls.py
+++ task_api/urls.py
@@ -5,16 +5,11 @@ from task_api import views
 
 router = DefaultRouter()
 router.register('profile', views.UserProfileViewSet)
-
-topic_router = DefaultRouter()
-topic_router.register('profile/<int:id>/topic', views.UserProfileTopicVeiwSet)
-
-task_router = DefaultRouter()
-task_router.register('task', views.UserProfileTaskVeiwSet)
+router.register('profile/<int:profile_id>/topic', views.UserProfileTopicVeiwSet)
+#router.register('profile/<int:profile_id>/topic/<int:topic_id>/task', views.UserProfileTaskVeiwSet)
 
 urlpatterns = [
     path('login/', views.UserLoginApiView.as_view()),
     path('', include(router.urls)),
-    path('', include(topic_router.urls)),
-    #path('profile/<int:id>/topic/<int:id>/', include(task_router.urls))
+
 ]
